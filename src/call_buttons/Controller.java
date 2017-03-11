d8541209b19fb28a2b3c709bb99bf7feb902b7a4
emailman <emailman@dtcc.edu>
2017-03-11 09:06:01 -0500

    better naming

--- src/call_buttons/Controller.java
+++ src/call_buttons/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package call_buttons;
 
 import javafx.scene.control.Label;
 
--- src/call_buttons/Main.java
+++ src/call_buttons/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package call_buttons;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
